7f2c0673778cb819a53d859ad7e233316258617b
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-14 11:39:30 +0200

    marker

--- js/marker.js
+++ js/marker.js
@@ -1,3 +1,49 @@
+class Marker {
+    constructor(color, amount) {
+        this.color = color;
+        this.amount = amount;
+    }
+
+    print(line) {
+        let content = document.getElementById('content');
+        let message = document.getElementById('message')
+        for (let i = 0; i < line.length; i++) {
+            if (this.amount != 0) {
+                content.innerHTML += line[i]
+                content.style.color = this.color
+                this.amount -= 0.5;
+
+                if (line[i] == ' ') {
+                    this.amount += 0.5;
+                }
+
+            } else {
+                message.innerHTML += "...необходимо пополнить чернила."
+                break
+            }
+
+        }
+    }
+}
+
+class FilledMarker extends Marker {
+    fill(amount) {
+        if (amount >= 100) {
+            amount = 100;
+        } else {
+            this.amount += amount;
+        }
+    }
+
+}
+
+let text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+
+let marker = new FilledMarker('green', 5);
+
+marker.fill(2)
+
+marker.print(text)
 
 
 /* Реализовать класс, описывающий простой маркер. В классе должны быть следующие компоненты:
